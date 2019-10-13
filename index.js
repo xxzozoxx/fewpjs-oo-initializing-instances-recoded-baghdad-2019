afda1778d864cd9df94e4deea42ee6e3de8ab5c2
Zahraa  Jalaa <zahraa.jalaa99@gmail.com>
2019-10-13 21:08:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 // Write your code here
 class Breakfast {
-  constructor()
+  constructor(food,drink){
+    this.food = food
+    this.drink =drink
+  }
+}
+class Lunch {
+  constructor(salad,soup,drink){
+    this.salad = salad
+    this.soup = soup
+    this.drink = drink
+  }
+}
+class Dinner {
+  constructor(salad,entree,dessert){
+    this.salad = salad
+    this.entree = entree
+    this._dessert = dessert
+  }
 }
